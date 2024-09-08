44e9dd6692e1d8775778ccbbff5e3fcd83c7d845
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-09-08 14:12:50 +0530

    Page Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import {  BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "./Pages/HomePage";
+import ArticlePage from "./Pages/ArticlePage";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Loader from "./Pages/Loader";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className='text-3xl font bold'>Hello EveryOne</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/article/:id" element={<ArticlePage/>} />
+        <Route path="*" element={<Loader/>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
